3b1968b1c063d6a61b13aba76bd6a84bef5b1e97
S. Otzoy <otzoy97@hotmail.com>
2020-06-07 21:02:41 -0600

    now all ops are condensated in a single class

--- expression.py
+++ expression.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
-class ARITHMETIC(Enum):
-    '''Arithmetic operators'''
+class Operator(Enum):
+    '''All operators'''
     PLUS = 1        # +
     MINUS = 2       # -
     TIMES = 3       # *
@@ -9,43 +9,61 @@ class ARITHMETIC(Enum):
     REMAINDER = 5   # %
     NEGATIVE = 6    # -
     ABS = 7         # abs
+    NOT = 8         # !
+    AND = 9         # &&
+    XOR = 10        # xor
+    OR = 11         # ||
+    NOTBW = 12      # ~ 
+    ANDBW = 13      # &
+    ORBW = 14       # |
+    XORBW = 15      # ^
+    SHL = 16        # <<
+    SHR = 17        # >>
+    EQ = 18         # =
+    NEQ = 19        # !=
+    GR = 20         # >
+    GRE = 21        # >=
+    LS = 22         # <
+    LSE = 23        # <=
+    AMP = 24        # &
+    CINT = 25       # int
+    CFLOAT = 26     # float
+    CSTRING = 27    # string
+    READ = 28       # read()
+    ARRAY = 29      # array()
 
-class LOGIC(Enum):
-    '''Logic (booleans) operators'''
-    NOT = 1         # !
-    AND = 2         # &&
-    XOR = 3         # xor
-    OR = 4          # ||
+class Expression:
+    '''
+        Abstract class for expressions
+    '''
 
-class BYTE_(Enum):
-    '''Byte operators'''
-    NOTBW = 1       # ~ 
-    ANDBW = 2       # &
-    ORBW = 3        # |
-    XORBW = 4       # ^
-    SHL = 5         # <<
-    SHR = 6         # >>
+class StringExpression(Expression):
+    '''
+    '''
+    def __init__(self, value):
+        self.value = value
 
-class RELATIONAL(Enum):
-    '''Relational operators'''
-    EQ = 1          # =
-    NEQ = 2         # !=
-    GR = 3          # >
-    GRE = 4         # >=
-    LS = 5          # <
-    LSE = 6         # <=
 
-class POINTER(Enum):
-    '''Pointer operator'''
-    AMP = 1         # &
+class FloatExpression(Expression):
+    '''
+    '''
+    def __init__(self,value):
+        self.value = value
 
-class CAST(Enum):
-    '''Casting operators'''
-    INT = 1         # int
-    FLOAT = 2       # float
-    STRING = 3      # string
+class IntegerExpression(Expression):
+    '''
+    '''
+    def __init__(self,value):
+        self.value = value
 
-class NATIVE(Enum):
-    '''Some native functions that works as operators'''
-    READ = 1        # read()
-    ARRAY = 2       # array()
+class ArrayExpression(Expression):
+    '''
+    '''
+
+class OperationExpression(Expression):
+    '''
+    '''
+    def __init__(self, op, e1, e2 = None):
+        self.e1 = e1
+        self.e2 = e2
+        self.op = op 
